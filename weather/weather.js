8c8d5b420fbb8075ab8f83b6c2b318810d6ebf24
torieforet <77910527+torieforet@users.noreply.github.com>
2021-04-08 20:36:48 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemapUrl = 'https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
@@ -14,6 +14,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
     style: function(feature){
       var alertColor = 'orange';
       if (feature.properties.severity === 'Severe') alertColor = 'red';
+      if (feature.properties.severity === 'Minor') alertColor = 'yellow';
       return { color: alertColor }
     },
     onEachFeature: function(feature, layer) {
